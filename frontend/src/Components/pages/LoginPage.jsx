2ca943d2cf63a0742cf5c8933f734628237e36c3
entorus <entorus@yandex.ru>
2026-06-04 23:10:42 +0900

    add styles make formik work

--- frontend/src/Components/pages/LoginPage.jsx
+++ frontend/src/Components/pages/LoginPage.jsx
@@ -1,14 +1,39 @@
-import { Formik } from 'formik'
+import { Form, Field, Formik } from 'formik'
 
-const LoginPage = () => (
-  <>
-    <Formik>
-      <form>
-        <input type="email" name="email" />
-        <input type="password" name="password" />
-        <button type="submit">Submit</button>
-      </form>
+const LoginPage = () => {
+  const handleSubmit = (values, actions) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2))
+      actions.setSubmitting(false)
+    }, 1000)
+  }
+  return (
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      onSubmit={handleSubmit}
+    >
+      {() => (
+        <Form>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <Field
+              type="email"
+              name="email"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <Field
+              type="password"
+              name="password"
+              className="form-control"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </Form>
+      )}
     </Formik>
-  </>
-)
+  )
+}
 export default LoginPage
